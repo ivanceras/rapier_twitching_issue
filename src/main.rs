3b4cd51ca31842853ff7bcfe721adbb1763d1b85
Jovansonlee Cesar <ivanceras@gmail.com>
2024-07-08 15:16:15 +0800

    fix: margin

--- src/main.rs
+++ src/main.rs
@@ -66,8 +66,8 @@ fn show_instructions(mut commands: Commands) {
         ])
         .with_style(Style {
             position_type: PositionType::Absolute,
-            top: Val::Px(5.0),
-            left: Val::Px(5.0),
+            top: Val::Px(10.0),
+            left: Val::Px(10.0),
             ..default()
         }),
     ));
@@ -88,8 +88,8 @@ fn show_instructions(mut commands: Commands) {
         ])
         .with_style(Style {
             position_type: PositionType::Absolute,
-            bottom: Val::Px(5.0),
-            left: Val::Px(5.0),
+            bottom: Val::Px(10.0),
+            left: Val::Px(10.0),
             ..default()
         }),
     ));
